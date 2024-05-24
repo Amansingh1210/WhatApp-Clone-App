1bfb221f5ce1aff62f678bab8d34e071c002c72c
Aman Singh <singhaman38809@gmail.com>
2024-05-24 22:15:58 +0530

    login ui and Authgoogle complete

--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
  import express from 'express'
  import dotenv from 'dotenv';
  import cors from 'cors';
+import AuthRoutes from './routes/AuthRoutes.js ';
 
 dotenv.config();
 const app = express();
@@ -8,8 +9,10 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-const port = process.env.PORT;
+app.use('/api/auth',AuthRoutes)
 
+
+const port = process.env.PORT;
 const server = app.listen(port,(req,res)=>{
     console.log(`server started on ${port}`);
 })
